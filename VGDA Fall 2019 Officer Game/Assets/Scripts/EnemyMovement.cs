34af4ba5820589e5afc67618bb617eecf099b0de
Jessica Wei <jessicawei9@gmail.com>
2019-08-31 15:52:30 -0700

    rb components while in air set up

--- VGDA Fall 2019 Officer Game/Assets/Scripts/EnemyMovement.cs
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    // Object References
+    private Rigidbody rb;
+
+    // Flying checkers & defaults
+    [SerializeField] private bool isFlying = false;
+    private bool flying = false;
+    private float oDrag;
+    private float oADrag;
+
+    // Set up Enemy Object first
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        oDrag = rb.drag;
+        oADrag = rb.angularDrag;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFlying && !flying) {
+            rb.useGravity = false;
+            rb.drag = 0;
+            rb.angularDrag = 0;
+            flying = true;
+        }
+        else if (!isFlying && flying){
+            rb.useGravity = true;
+            rb.drag = oDrag;
+            rb.angularDrag = oADrag;
+            flying = false;
+        }
     }
 }
