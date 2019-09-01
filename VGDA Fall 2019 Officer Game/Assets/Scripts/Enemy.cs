942bf737c11cd7fef064038514619821445efcc3
Jessica Wei <jessicawei9@gmail.com>
2019-09-01 00:02:30 -0700

    Enemy abstract class written, will eventually delete enemy movement

---
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] protected float health;
+    [SerializeField] protected float damage;
+    [SerializeField] protected float attackFrequency;
+
+    [HideInInspector] protected GameObject Player;
+    [HideInInspector] protected Animator anim;
+    [HideInInspector] protected Rigidbody rb;
+
+    protected abstract void attack();
+    protected abstract void move();
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start() {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    protected virtual void begoned() {
+        if (health <= 0f) {
+            // Death animation here
+            Destroy(this);
+        }
+    }
+}
+    
+
--- VGDA Fall 2019 Officer Game/Assets/Scripts/EnemyMovement.cs
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/EnemyMovement.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    // Uniqueify Enemies
+
     // Object References
     private Rigidbody rb;
+    private GameObject Player;
 
     // Flying checkers & defaults
     [SerializeField] private bool isFlying = false;
-    private bool flying = false;
     private float oDrag;
     private float oADrag;
+
+    // Prevention of chaging the same values
+    private bool flying = false;
+    private Transform lastPlayerTransform;
 
     // Set up Enemy Object first
     void Awake()
@@ -24,13 +30,17 @@ public class EnemyMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-
+        Player = GameObject.FindGameObjectWithTag("Player");
+        lastPlayerTransform = Player.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Player.transform != lastPlayerTransform)
+            transform.LookAt(Player.transform);
+
+
         if (isFlying && !flying) {
             rb.useGravity = false;
             rb.drag = 0;
