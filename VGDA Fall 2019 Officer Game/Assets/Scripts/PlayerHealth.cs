c24acb90d705b87bb94ac719482fe691e726b03e
nguyenhanson1 <nguyenhanson1@gmail.com>
2019-09-07 20:58:31 -0700

    did player heaslth and learned events and delegates

--- VGDA Fall 2019 Officer Game/Assets/Scripts/PlayerHealth.cs
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/PlayerHealth.cs
@@ -2,8 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : Health
+public class PlayerHealth : MonoBehaviour
 {
-   
-    
+    [SerializeField]
+    private int healthToSet;
+
+    private Health shipHealth = new Health() ;
+
+    private void Awake()
+    {
+        shipHealth.HealthTotal = healthToSet;
+    }
+
+    private void OnEnable()
+    {
+        Health.OnDeath += DestroyShip;
+    }
+    private void OnDisable()
+    {
+        Health.OnDeath -= DestroyShip;
+    }
+    private void DestroyShip(Health ship)
+    {
+        if(shipHealth == ship)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void subtractHealth(int damage)
+    {
+        shipHealth.HealthTotal -= damage;
+    }
+    private void addHealth(int damage)
+    {
+        shipHealth.HealthTotal += damage;
+    }
 }
