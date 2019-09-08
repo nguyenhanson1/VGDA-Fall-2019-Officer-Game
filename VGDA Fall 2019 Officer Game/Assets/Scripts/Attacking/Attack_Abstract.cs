d0a0ea989351498cd3d7a1efe022ca9856fe55e8
jusin-PC\Just <justincheung6@gmail.com>
2019-09-08 13:15:46 -0700

    -Created and finished Attack_Abstract script -Created and finished ObjectPooler script -Created Attack_Player script (empty)

---
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/Attacking/Attack_Abstract.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class Attack_Abstract : MonoBehaviour
+{
+    private ObjectPooler bulletPool;
+
+    public void Shoot(Vector3? position = null, Quaternion? rotation = null)
+    {
+        GameObject bullet = bulletPool.GetGenericBullet();
+        if(bullet != null)
+        {
+            bullet.transform.position = position.HasValue ? position.Value : transform.position;
+            bullet.transform.rotation = rotation.HasValue ? rotation.Value : transform.rotation;
+            bullet.SetActive(true);
+        }
+    }
+}
---
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/Attacking/ObjectPooler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectPooler : MonoBehaviour
+{
+    //Slap this boi onto the empty gameObject to hold the bullets
+
+    public List<GameObject> genericBulletPool = new List<GameObject>();
+    public GameObject genericBulletPrefab = null;
+    public int totalGenericBullets = 50;
+
+    private int instantiatedGenericBullets = 0;
+
+    private void Start()
+    {
+        genericBulletPool = new List<GameObject>();
+        for(int i = 0; i < totalGenericBullets; i++)
+        {
+            GameObject obj = Instantiate(genericBulletPrefab);
+            obj.transform.parent = transform;
+            obj.SetActive(false);
+            genericBulletPool.Add(obj);
+
+        }
+    }
+
+    public GameObject GetGenericBullet()
+    {
+        for(int i = 0; i < genericBulletPool.Count; i++)
+        {
+            if (!genericBulletPool[i].activeInHierarchy)
+                return genericBulletPool[i];
+        }
+
+        GameObject obj = Instantiate(genericBulletPrefab);
+        obj.transform.parent = transform;
+
+        obj.SetActive(false);
+        genericBulletPool.Add(obj);
+        instantiatedGenericBullets++;
+
+        return genericBulletPool[totalGenericBullets - 1];
+    }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("There are " + instantiatedGenericBullets + " new bullets in the " + this.gameObject.name + " gameObject.");
+    }
+}
