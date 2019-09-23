a85a2171bbc1bf8cac3c47fde8c1b968580d154a
JustinCheung6 <justincheung6@gmail.com>
2019-09-23 16:53:12 -0700

    -Updated MoveCursor script to use delegates and orginized its functions

--- VGDA Fall 2019 Officer Game/Assets/Scripts/Menu Scripts/MoveCursor.cs
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/Menu Scripts/MoveCursor.cs
@@ -23,80 +23,116 @@ public class MoveCursor : MonoBehaviour
 
     private RectTransform cursor = null;
 
-    private void Awake()
+    private void OnEnable()
     {
-        cursor = GetComponent<RectTransform>();
+        GameManager.UpdateOccurred += FunctionHandler;
 
-        //Hides cursor if using mouse controls.
-        if (!controllerEnabled)
+    }
+    //Removes need of adding all 4 functions to delegate
+    private void FunctionHandler()
+    {
+        if (controllerEnabled)
+        {
+            ShowCursor();
+            ControllerControls();
+        }
+        else
+        {
+            HideCursor();
+            MouseControls();
+        }
+        if (Input.GetButtonDown("CenterCursor"))
+            CenterCursor();
+    }
+
+    //Hides cursor if using mouse controls.
+    private void HideCursor()
+    {
+        if (Cursor.visible == true)
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
             Debug.Log("Press ctrl + P to toggle the play button.  Ctrl + shift + P to pause.");
         }
     }
-
-    private void Update()
+    //Shows cursor if not using mouse controls.
+    private void ShowCursor()
     {
-        //Controller controls
+        if(Cursor.visible == false)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    //Returns the Cursor to center screen.
+    private void CenterCursor()
+    {
+        cursor.position = new Vector2(canvas.rect.width / 2, canvas.rect.height / 2);
+    }
+    //Set position of Cursor depending on Horizontal and Vertical axis inputs.
+    private void ControllerControls()
+    {
         if (controllerEnabled)
         {
-            //Create new Vector to easily change reticle position
-            Vector2 newPosition = new Vector2(GetX_Controller(),GetY_Controller());
-
-            //Moves cursor to the calculated position
-            cursor.position = newPosition;
-        }
-        //Mouse Controls
-        else
-        {
-            //Create new Vector to easily change reticle position
-            Vector2 newPosition = cursor.position;
-            newPosition.x += Input.GetAxis("Mouse X") * Time.deltaTime * speed;
-            newPosition.y += Input.GetAxis("Mouse Y") * Time.deltaTime * speed;
-
-            newPosition.x = Mathf.Clamp(newPosition.x, xBorders[0], canvas.rect.width - xBorders[1]);
-            newPosition.y = Mathf.Clamp(newPosition.y, yBorders[0], canvas.rect.height - yBorders[1]);
-
-            //Sets origin to the middle of the screen, and moves cursor to position needed
-            cursor.position = newPosition;
-        }
-
-        //Recenters Cursor when button is pressed
-        if (Input.GetButtonDown("CenterCursor"))
-            cursor.position = new Vector2(canvas.rect.width / 2, canvas.rect.height / 2);
-    }
-
-    private float GetX_Controller()
-    {
-        if (keepCursorCentered)
-        {
-            if (Input.GetAxis("Horizontal") > 0)
-                return (canvas.rect.width / 2) + (Input.GetAxis("Horizontal") * ((canvas.rect.width / 2) - xBorders[1]));
+            if (keepCursorCentered)
+                cursor.position = new Vector2(GetX_Centered(), GetY_Centered());
             else
-                return (canvas.rect.width / 2) + (Input.GetAxis("Horizontal") * ((canvas.rect.width / 2) - xBorders[0]));
-        }
-        else
-        {
-            float xPos = cursor.position.x;
-            xPos += Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-            return Mathf.Clamp(xPos, xBorders[0], canvas.rect.width - xBorders[1]);
+                cursor.position = new Vector2(GetX_Free(), GetY_Free());
         }
     }
-    private float GetY_Controller()
+    //Centered Functions return the cursor to center screen if there's no input
+    private float GetX_Centered()
     {
-        if (keepCursorCentered)
-        {
-            if (Input.GetAxis("Vertical") > 0)
-                return (canvas.rect.height / 2) + Input.GetAxis("Vertical") * ((canvas.rect.height / 2) - yBorders[1]);
-            else
-                return (canvas.rect.height / 2) + Input.GetAxis("Vertical") * ((canvas.rect.height / 2) - yBorders[0]);
-        }
-        else
-        {
-            float yPos = cursor.position.y;
-            yPos += Input.GetAxis("Vertical") * Time.deltaTime * speed;
-            return Mathf.Clamp(yPos, yBorders[0], canvas.rect.height - yBorders[1]);
-        }
+        float centerScreen = (canvas.rect.width / 2);
+        //Times input by centerScreen to move Cursor through the other half of screen (0 = center, 1 = edge of screen)
+        float input = Input.GetAxis("Horizontal") * centerScreen;
+        //Have borders to accomodate HUD or other restrictions
+        float border = (input > 0) ? xBorders[1] : xBorders[0];
+
+        return centerScreen + input - border;
+    }
+    private float GetY_Centered()
+    {
+        float centerScreen = (canvas.rect.height / 2);
+        //Times input by centerScreen to move Cursor through the other half of screen (0 = center, 1 = edge of screen)
+        float input = Input.GetAxis("Vertical") * centerScreen;
+        //Have borders to accomodate HUD or other restrictions
+        float border = (input > 0) ? yBorders[1] : yBorders[0];
+
+        return centerScreen + input - border;
+    }
+    //Free Functions let's Cursor travel around screen
+    private float GetX_Free()
+    {
+        float xPos = cursor.position.x;
+        //Move Cursor position
+        xPos += Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        //Clamp max position to screen borders
+        xPos = Mathf.Clamp(xPos, xBorders[0], canvas.rect.width - xBorders[1]);
+        return xPos;
+    }
+    private float GetY_Free()
+    {
+        float yPos = cursor.position.y;
+        //Move Cursor position
+        yPos += Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        //Clamp max position to screen borders
+        yPos = Mathf.Clamp(yPos, yBorders[0], canvas.rect.height - yBorders[1]);
+        return yPos;
+    }
+    //Have mouse replace the cursor
+    private void MouseControls()
+    {
+        //Create new Vector to easily change reticle position
+        Vector2 newPosition = cursor.position;
+        newPosition.x += Input.GetAxis("Mouse X") * Time.deltaTime * speed;
+        newPosition.y += Input.GetAxis("Mouse Y") * Time.deltaTime * speed;
+
+        //Clamp max positions to screen borders
+        newPosition.x = Mathf.Clamp(newPosition.x, xBorders[0], canvas.rect.width - xBorders[1]);
+        newPosition.y = Mathf.Clamp(newPosition.y, yBorders[0], canvas.rect.height - yBorders[1]);
+
+        //Sets origin to the middle of the screen, and moves cursor to position needed
+        cursor.position = newPosition;
     }
 }
