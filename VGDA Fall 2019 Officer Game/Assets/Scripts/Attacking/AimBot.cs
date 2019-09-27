fd27c3ea07a35b38ee1b8f3876c6881655dc9b7c
JustinCheung6 <justincheung6@gmail.com>
2019-09-27 10:22:17 -0700

    -fixed AimBot script (Everything should be fine now)

--- VGDA Fall 2019 Officer Game/Assets/Scripts/Attacking/AimBot.cs
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/Attacking/AimBot.cs
@@ -25,7 +25,7 @@ public class AimBot : MonoBehaviour
 
         float distance = bullet.Speed * bullet.DespawnTime + 20f;
         //Shoot raycast and return gameObject if hit something with wanted layer
-        if (Physics.Raycast(cursorSight, out RaycastHit hit, distance, bullet.Targets.value, QueryTriggerInteraction.Collide))
+        if (Physics.Raycast(cursorSight, out RaycastHit hit, distance, ~0, QueryTriggerInteraction.Collide))
             return hit.transform.gameObject;
         return null;
     }
