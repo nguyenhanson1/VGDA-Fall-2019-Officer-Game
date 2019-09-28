4541b3e7d3ed9307d3b0d4ddf72dd85379be6879
nguyenhanson1 <nguyenhanson1@gmail.com>
2019-09-27 21:08:24 -0700

    ground enemy patrol

---
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/GroundAi/GroundMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class GroundMovement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject[] waypoints;
+    private void Start()
+    {
+        agent.SetDestination(waypoints[0].transform.position);
+    }
+    void Update()
+    {
+        if (Vector3.Distance(agent.destination,transform.position) <= 1f)
+        {
+            int target = Random.Range(0, waypoints.Length);
+            agent.SetDestination(waypoints[target].transform.position);
+        }
+        
+
+    }
+    void LookAround()
+    {
+        for(float timer = 5; timer > 0; timer -= Time.deltaTime)
+        {
+              
+        }
+    }
+}
