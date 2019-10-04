62d9bfb8b122aa41cc300af7f4ff30e5a008a092
JustinCheung6 <justincheung6@gmail.com>
2019-10-04 16:49:34 -0700

    -Creating Demo Level mihigy with what we have

---
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/Testing - Justin/PracticeTarget.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeTarget : MonoBehaviour
+{
+    public Vector3 start;
+    public Vector3 end;
+    public float startTime;
+    public float speed;
+    public float distance;
+
+    private void OnEnable()
+    {
+        GameManager.UpdateOccurred += GoTo;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.UpdateOccurred -= GoTo;
+    }
+    private void GoTo()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, end, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, end) == 0)
+        {
+            transform.position = start;
+        }
+            
+    }
+}
---
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/Testing - Justin/ShootingGallery.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingGallery : MonoBehaviour
+{
+    [SerializeField] private Transform endPoint = null;
+    [SerializeField] private int speed = 20;
+    [SerializeField] private GameObject targetPrefab = null;
+
+    [SerializeField] private int amtTargets = 10;
+    [SerializeField] private List<GameObject> targets = new List<GameObject>();
+
+    private void OnEnable()
+    {
+        GameManager.StartOccurred += fillObjectPool;
+    }
+    private void OnDisable()
+    {
+        GameManager.StartOccurred -= fillObjectPool;
+    }
+
+    //Fill the ObjectPool with bullets
+    private void fillObjectPool()
+    {
+        //Reset and fill list with bullets
+        targets = new List<GameObject>();
+        for (int i = 0; i < amtTargets; i++)
+        {
+            MakeStartTarget(i);
+        }
+    }
+
+    private void MakeStartTarget(float iteration)
+    {
+        GameObject obj = Instantiate(targetPrefab);
+
+        //Put block on the row
+        Debug.Log((iteration / (amtTargets)));
+        float percentage = (iteration / amtTargets);
+        Vector3 translation = (endPoint.position - transform.position) * percentage;
+        Vector3 xyz = transform.position + translation;
+        obj.transform.position = xyz;
+
+        ChangeVariables(obj);
+        obj.transform.parent = transform;
+        targets.Add(obj);
+
+    }
+
+    private void ChangeVariables(GameObject obj)
+    {
+        PracticeTarget target = obj.GetComponent<PracticeTarget>();
+        target.speed = speed;
+        target.start = transform.position;
+        target.end = endPoint.position;
+
+    }
+}
