86452534f0721e1f6d5e357313fb5a4008e42164
Chloe Culver <c.culver831@gmail.com>
2019-10-10 23:22:16 -0700

    Idk but hoes sad :(

--- VGDA Fall 2019 Officer Game/Assets/Addforce.cs
+++ VGDA Fall 2019 Officer Game/Assets/Addforce.cs
@@ -6,24 +6,35 @@ public class Addforce : MonoBehaviour
 {
     [SerializeField] private RectTransform cursor = null;
     [SerializeField] private Camera cursorCamera = null;
-    [SerializeField] private Vector3 offset = new Vector3(0, 0, 0);
-    [SerializeField] public GameObject Player;
 
+
+    public float rotSpeed = 1.0f;
     public float movementSpeed = 1.0f;
-    public int invert = -1;
-
 
     private void Update()
     {
-        Vector3 move = Vector3.Lerp(transform.position, cursorCamera.ScreenToWorldPoint(cursor.position) + offset, Time.deltaTime * movementSpeed);
+
+        Vector3 move = Vector3.Lerp(transform.position, cursorCamera.ScreenToWorldPoint(cursor.position), Time.deltaTime * movementSpeed);
         move.z = cursorCamera.transform.position.z + 1f;
 
         transform.position = move;
-        if(cursor.position.x == Screen.width || cursor.position.y == Screen.height )
+        if (cursor.position.y == Screen.height )
         {
-            Vector3 PlayerRotate = Vector3.Lerp(transform.position, cursorCamera.ScreenToWorldPoint(cursor.position) + offset, Time.deltaTime * movementSpeed);
-            PlayerRotate.x = Player.transform.position.x* movementSpeed;
-            PlayerRotate.y = Player.transform.position.y * movementSpeed;
+            Debug.Log("We on da Top");
+            Vector3 direction = cursorCamera.ScreenToWorldPoint(cursor.position );
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.left);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, rotSpeed * Time.deltaTime);
+
+
+        }
+        if(cursor.position.x == Screen.width)
+        {
+            Debug.Log("We on da Sides");
+            Vector3 direction = cursorCamera.ScreenToWorldPoint(cursor.position);
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, rotSpeed * Time.deltaTime);
         }
 
     }
