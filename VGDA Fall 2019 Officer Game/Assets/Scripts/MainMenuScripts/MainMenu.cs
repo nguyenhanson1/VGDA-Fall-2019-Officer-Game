56653dd08954bb4579a363bc2f775dbbd9dbe5d5
Chloe Culver <c.culver831@gmail.com>
2019-11-08 14:41:54 -0800

    Fixing Camera Animation on Main Menu

---
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/MainMenuScripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    //public static bool PlayerSelect = false;
+  
+    public GameObject Main_Menu;
+    public static bool MainM = true;
+    private Animator Camera;
+    public GameObject Cam;
+    private void Start()
+    {
+        Cam = GameObject.Find("Main Camera");
+        Camera = Cam.GetComponent<Animator>();
+    }
+    public void Play()
+    {
+        Camera.Play("Camera_Pan");
+        StartCoroutine(ChangeMenu());
+        Main_Menu.SetActive(false);
+
+        StopCoroutine(ChangeMenu());
+
+    }
+
+    IEnumerator ChangeMenu()
+    {
+
+        yield return new WaitForSeconds(3);
+  
+
+    }
+}
