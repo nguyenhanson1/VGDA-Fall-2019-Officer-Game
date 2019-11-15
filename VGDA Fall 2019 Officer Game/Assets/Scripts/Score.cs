6601166347fe68b9a464f5ab66c3089b9c9af747
nguyenhanson1 <nguyenhanson1@gmail.com>
2019-11-14 17:44:12 -0800

    update Progress

--- VGDA Fall 2019 Officer Game/Assets/Scripts/Score.cs
+++ VGDA Fall 2019 Officer Game/Assets/Scripts/Score.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-
 public class Score : MonoBehaviour {
 
     public Text scoreText;
     public static int score = 0;
-
+    private void Awake()
+    {
+        scoreText = GetComponent<Text>();
+    }
     // Update is called once per frame
     void Update() {
         scoreText.text = "Score: " + score;
